914ac921206989fc4e845a8b1d8b9d035f847aab
Michael Manis <michaelmanis@tutanota.com>
2024-08-09 22:24:14 -0400

    start tailwind

--- web/src/js/index.js
+++ web/src/js/index.js
@@ -10,13 +10,13 @@
 //     GNU Affero General Public License for more details.
 //     You should have received a copy of the GNU Affero General Public License
 //     along with this program.  If not, see <https://www.gnu.org/licenses/>.
-const inputText = document.querySelector("#input-text");
-const chatArea = document.querySelector("#div-chat-area");
-const chatInputArea = document.querySelector("#chat-input-area");
-const sendBtn = document.querySelector("#btn-send");
-const tokenModal = document.querySelector("#token-modal");
-const tokenBtn = document.querySelector("#btn-token");
-const tokenInput = document.querySelector("#input-token");
+let inputText;
+let chatArea;
+let chatInputArea;
+let sendBtn;
+let tokenModal;
+let tokenBtn;
+let tokenInput;
 
 const conversation = [];
 
@@ -24,6 +24,7 @@ const postChat = async () => {
   sendBtn.setAttribute("disabled", "");
 
   recordMessage("user", inputText.value)
+  inputText.value = "";
 
   const response = await fetch("/api/converse", {
     method: "POST",
@@ -45,8 +46,18 @@ const recordMessage = (role, text) => {
 
   const username = document.createElement("inline");
   username.classList.add(role);
-  const message = document.createElement("p");
-  message.classList.add(role)
+  const message = document.createElement("div");
+
+  let messageClasses;
+  if (role === "user") {
+    username.innerText = "You";
+    messageClasses = ["mx-2", "self-end", "bg-send-bg", "text-send-text", "rounded-lg", "px-4", "py-2"];
+  } else {
+    username.innerText = "AI";
+    messageClasses = ["px-8", "py-2", "mx-2", "bg-receive-bg", "rounded-lg", "text-receive-text"];
+  }
+
+  message.classList.add(...messageClasses);
   message.innerText = text;
 
   chatArea.appendChild(username);
@@ -62,6 +73,8 @@ const postToken = async () => {
     headers: { "Content-Type": "application/json" }
   });
 
+  tokenInput.value = "";
+
   if (tokenCheck.status === 200) {
     tokenModal.style.display = "none";
     chatInputArea.style.display = "flex";
@@ -76,14 +89,30 @@ const authenticated = async () => {
   return authCheck.status === 200;
 };
 
+const onEnterPressed = async (btn, e) => {
+  if (e.key !== "Enter") return;
+
+  btn.dispatchEvent(new Event("click", { bubbles: true }));
+}
+
 document.addEventListener('DOMContentLoaded', async () => {
+  inputText = document.querySelector("#input-text");
+  chatArea = document.querySelector("#chat-area");
+  chatInputArea = document.querySelector("#chat-input-area");
+  sendBtn = document.querySelector("#btn-send");
+  tokenModal = document.querySelector("#token-modal");
+  tokenBtn = document.querySelector("#btn-token");
+  tokenInput = document.querySelector("#input-token");
+  
   if (await authenticated()) {
-    // authed
+    tokenModal.style.display = "none";
   } else {
     tokenModal.style.display = "flex";
     chatInputArea.style.display = "none";
   }
-
-  document.querySelector("#btn-send").addEventListener("click", postChat);
-  document.querySelector("#btn-token").addEventListener("click", postToken);
+  tokenBtn.addEventListener("click", postToken);
+  sendBtn.addEventListener("click", postChat);
+  inputText.addEventListener("keydown", async (e) => { await onEnterPressed(sendBtn, e) });
+  tokenInput.addEventListener("keydown", async (e) => { await onEnterPressed(tokenBtn, e) });
+  inputText.focus();
 });
---
+++ web/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{js,html}"],
+  theme: {
+    extend: {
+      colors: {
+        "send-bg": "#0b93f6",
+        "send-text": "#fff",
+        "receive-bg": "#E5E5EA",
+        "receive-text": "#000"
+      },
+    },
+  },
+  plugins: [],
+}
